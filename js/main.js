0ed29ab38f1dd576d9ee5348c0a8f5220d75ce9e
petro_pasteruk <petropasteruk@gmail.com>
2020-08-12 15:37:31 +0300

    asdasD

--- js/main.js
+++ js/main.js
@@ -764,7 +764,7 @@ $(document).ready(function () {
                 </div>`;
 
         oldContactItem.innerHTML = `<a title="click to visit map" target="_blank" class="maplink" href="https://goo.gl/maps/tymxCCCDFqL2">St. Augustine, FL 32084</a>`;
-        newContactItem.innerHTML = `<a title="click to visit map" target="_blank" class=block22__right-link"maplink" href="https://goo.gl/maps/4gJZzS48vH7bPs9s5">100 Saint George street, St. Augustine, Florida</a>`;
+        newContactItem.innerHTML = `<a title="click to visit map" target="_blank" class="block22__right-link maplink" href="https://goo.gl/maps/4gJZzS48vH7bPs9s5">100 Saint George street, St. Augustine, Florida</a>`;
 
         oldMapParent.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2970.6332361804293!2d-87.62767486487378!3d41.87923682399129!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e2ca33f58ffd1%3A0xf06bc2d1f0d09729!2zMjExIFMgU3RhdGUgU3QsIENoaWNhZ28sIElMIDYwNjA0LCDQodCo0JA!5e0!3m2!1sru!2sua!4v1555176889706!5m2!1sru!2sua" width="100%" height="340" frameborder="0" style="border:0;max-width:100%" allowfullscreen></iframe>`;
         newMapParent.innerHTML = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3458.9461479293295!2d-81.31547958492514!3d29.894652282588197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e42795ef01c2bd%3A0x8753e82fcf3752db!2zMTAwIFN0IEdlb3JnZSBTdCwgU3QuIEF1Z3VzdGluZSwgRkwgMzIwODQsINCh0L_QvtC70YPRh9C10L3RliDQqNGC0LDRgtC4INCQ0LzQtdGA0LjQutC4!5e0!3m2!1suk!2sua!4v1596539385660!5m2!1suk!2sua" width="100%" height="340" frameborder="0" style="border:0;max-width:100%" allowfullscreen></iframe>`;
